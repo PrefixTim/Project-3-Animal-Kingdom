c2c7f146830700e7d1b785a094944e8a49912a12
PrefixTim <developer.of.gift@gmail.com>
2020-03-18 14:40:54 -0700

    equals

--- src/main/java/animals/Animal.java
+++ src/main/java/animals/Animal.java
@@ -3,75 +3,69 @@ package animals;/* ADD THE EQUALS METHOD AND IMPLEMENT THE COMPARABLE INTERFACE 
 
 public abstract class Animal implements Comparable<Animal> {
 
-	private int id;
-	private String name;
-	private BirthType birthType;
+    public static final String ANIMAL_DESCRIPTION = "animals.Animal";
+    private int id;
+    private String name;
+    private BirthType birthType;
 
-	public static final String ANIMAL_DESCRIPTION = "animals.Animal";
+    public Animal(int id, String name, BirthType birthType) {
+        this.id = id;
+        this.name = name;
+        this.birthType = birthType;
+    }
 
-	public Animal(int id, String name, BirthType birthType) {
-		this.id = id;
-		this.name = name;
-		this.birthType = birthType;
-	}
 
-	
-	// information about birth type
-	public final boolean laysEggs() {
-		return birthType==BirthType.LAYS_EGGS;
-	}
-	public final boolean hasLiveBirth() {
-		return birthType==BirthType.LIVE_BIRTH;
-	}
-	public final BirthType getBirthType() {
-		return birthType;
-	}
+    // information about birth type
+    public final boolean laysEggs() {
+        return birthType == BirthType.LAYS_EGGS;
+    }
 
-	// getters (no setters provided)
-	public final int getId() {
-		return id;
-	}
+    public final boolean hasLiveBirth() {
+        return birthType == BirthType.LIVE_BIRTH;
+    }
 
-	public final String getName() {
-		return name;
-	}
+    public final BirthType getBirthType() {
+        return birthType;
+    }
 
-	//add equals method, equivalent if both name(ignore capitalization) and id are same
-	@Override
-	public boolean equals(Object o) {
-		if(o instanceof Animal)
-		{
-			if(((Animal)o).name.equalsIgnoreCase(this.name)
-			&& ((Animal)o).id==this.id)
-			{
-				return true;
-			}
-			else
-			{
-				return false;
-			}
-		}
-		else
-		{
-			return false;
-		}
-	}
-	//override compareTo method, Order name in alphabetic order
-	@Override
-	public int compareTo(Animal animal) {
-		return this.name.compareTo(animal.name);
-	}
+    // getters (no setters provided)
+    public final int getId() {
+        return id;
+    }
 
-	// toString method will create text with name, id, birth type, warm/cold blodded, and the description
-	@Override
-	public final String toString() {
-		return name + " (id=" + id + "):\t" + birthType.toString() + "\t" +
-				(isWarmBlooded() ? "Warm Blooded" : "Cold Blooded") + "\t" +
-				getDescription();
-	}
+    public final String getName() {
+        return name;
+    }
 
-	// abstract methods to implement in child classes
-	public abstract boolean isWarmBlooded();
-	public abstract String getDescription();
-	
+    //add equals method, equivalent if both name(ignore capitalization) and id are same
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Animal)) return false;
+        Animal animal = (Animal) o;
+        return getId() == animal.getId() &&
+                getName().equalsIgnoreCase(animal.getName());
+    }
+
+    //override compareTo method, Order name in alphabetic order
+    @Override
+    public int compareTo(Animal animal) {
+        return this.name.compareTo(animal.name);
+    }
+
+    // toString method will create text with name, id, birth type, warm/cold blodded, and the description
+    @Override
+    public final String toString() {
+        return name + " (id=" + id + "):\t" + birthType.toString() + "\t" +
+                (isWarmBlooded() ? "Warm Blooded" : "Cold Blooded") + "\t" +
+                getDescription();
+    }
+
+    // abstract methods to implement in child classes
+    public abstract boolean isWarmBlooded();
+
+    public abstract String getDescription();
+
 }
